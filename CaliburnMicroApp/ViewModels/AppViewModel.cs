dd0b1f02649fce3c1882bfe78a93702ad6fe21a7
Anastasiya <nastafirik@gmail.com>
2016-12-13 16:35:09 +0300

    one

---
+++ CaliburnMicroApp/ViewModels/AppViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Caliburn.Micro;
+
+namespace CaliburnMicroApp {
+    public class AppViewModel : PropertyChangedBase {
+
+        private int _count = 100;
+
+        public int Count {
+            get { return _count; }
+            set {
+                _count = value;
+                NotifyOfPropertyChange(() => Count);
+            }
+        }
+        public void IncrementCount(int delta) {
+            Count+=delta;
+        }
+        public bool CanIncrementCount {
+            get { return Count < 200; }
+            set { NotifyOfPropertyChange(() => CanIncrementCount); }
+        }
+    }
+}
